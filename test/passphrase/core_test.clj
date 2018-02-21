6632408715c4dcf681ecb2fef6f2d48578bdf452
Ali Raheem <ali.raheem@gmail.com>
2018-02-21 13:12:16 +0000

    Unit tests

--- test/passphrase/core_test.clj
+++ test/passphrase/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [passphrase.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest entropy-per-word-test
+  (testing "entropy-per-word"
+    (is (= (entropy-per-word 2) 1))
+    (is (= (entropy-per-word 5) 2))
+    (is (= (entropy-per-word 10) 3))))
+
+(deftest pick-from-list-test
+  (testing "entropy-per-word"
+    (let [coll '("hello" "test" "bye")]
+      (is (= (count (pick-from-list 1 coll)) 1))
+      (is (= (count (pick-from-list 2 coll)) 2))
+      (is (= (count (pick-from-list 3 coll)) 3))
+      (is (= (count (pick-from-list 10 coll)) 10)))))
